e1ad3190e124163c0e7e0e7fc03cfea6f43f0cf8
jean-philippe serafin <serafinjp@gmail.com>
2016-06-07 09:25:33 +0000

    Attach an `InternetGateway` to the `VPC`

--- stack/vpc.py
+++ stack/vpc.py
@@ -1,5 +1,11 @@
+from troposphere import (
+    Ref,
+)
+
 from troposphere.ec2 import (
+    InternetGateway,
     VPC,
+    VPCGatewayAttachment,
 )
 
 from .template import template
@@ -9,4 +15,20 @@ vpc = VPC(
     "Vpc",
     template=template,
     CidrBlock="10.0.0.0/16",
+)
+
+
+# Allow outgoing to outside VPC
+internet_gateway = InternetGateway(
+    "InternetGateway",
+    template=template,
+)
+
+
+# Attach Gateway to VPC
+VPCGatewayAttachment(
+    "GatewayAttachement",
+    template=template,
+    VpcId=Ref(vpc),
+    InternetGatewayId=Ref(internet_gateway),
 )
