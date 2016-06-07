389fd283c0e05a7c3ccdc871b2423a1d0f3b2280
jean-philippe serafin <serafinjp@gmail.com>
2016-06-07 11:40:53 +0000

    Add a `ECS` ami ids as a region mapping

--- stack/cluster.py
+++ stack/cluster.py
@@ -19,6 +19,13 @@ container_instance_type = Ref(template.add_parameter(Parameter(
 )))
 
 
+template.add_mapping("ECSRegionMap", {
+    "eu-west-1": {"AMI": "ami-4e6ffe3d"},
+    "us-east-1": {"AMI": "ami-8f7687e2"},
+    "us-west-2": {"AMI": "ami-84b44de4"},
+})
+
+
 # ECS cluster
 cluster = Cluster(
     "Cluster",
