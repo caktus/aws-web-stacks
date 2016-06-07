9230aa680d7cff3bcb4c5d4e56d3d12af6934f3f
jean-philippe serafin <serafinjp@gmail.com>
2016-06-07 13:10:38 +0000

    Add two public subnets (between two zones) that will hold the loadbalancer

--- stack/vpc.py
+++ stack/vpc.py
@@ -1,5 +1,7 @@
 from troposphere import (
+    AWS_REGION,
     GetAtt,
+    Join,
     Ref,
 )
 
@@ -90,4 +92,41 @@ nat_gateway = NatGateway(
     template=template,
     AllocationId=GetAtt(nat_ip, "AllocationId"),
     SubnetId=Ref(public_subnet),
+)
+
+
+# Holds load balancer
+loadbalancer_a_subnet_cidr = "10.0.2.0/24"
+loadbalancer_a_subnet = Subnet(
+    "LoadbalancerASubnet",
+    template=template,
+    VpcId=Ref(vpc),
+    CidrBlock=loadbalancer_a_subnet_cidr,
+    AvailabilityZone=Join("", [Ref(AWS_REGION), "a"]),
+)
+
+
+SubnetRouteTableAssociation(
+    "LoadbalancerASubnetRouteTableAssociation",
+    template=template,
+    RouteTableId=Ref(public_route_table),
+    SubnetId=Ref(loadbalancer_a_subnet),
+)
+
+
+loadbalancer_b_subnet_cidr = "10.0.3.0/24"
+loadbalancer_b_subnet = Subnet(
+    "LoadbalancerBSubnet",
+    template=template,
+    VpcId=Ref(vpc),
+    CidrBlock=loadbalancer_b_subnet_cidr,
+    AvailabilityZone=Join("", [Ref(AWS_REGION), "b"]),
+)
+
+
+SubnetRouteTableAssociation(
+    "LoadbalancerBSubnetRouteTableAssociation",
+    template=template,
+    RouteTableId=Ref(public_route_table),
+    SubnetId=Ref(loadbalancer_b_subnet),
 )
