6d04f0f924df11968b85aa2c885bde30cf6af597
jean-philippe serafin <serafinjp@gmail.com>
2016-06-07 13:04:22 +0000

    Add a public route table

--- stack/vpc.py
+++ stack/vpc.py
@@ -4,6 +4,8 @@ from troposphere import (
 
 from troposphere.ec2 import (
     InternetGateway,
+    Route,
+    RouteTable,
     VPC,
     VPCGatewayAttachment,
 )
@@ -31,4 +33,21 @@ VPCGatewayAttachment(
     template=template,
     VpcId=Ref(vpc),
     InternetGatewayId=Ref(internet_gateway),
+)
+
+
+# Public route table
+public_route_table = RouteTable(
+    "PublicRouteTable",
+    template=template,
+    VpcId=Ref(vpc),
+)
+
+
+public_route = Route(
+    "PublicRoute",
+    template=template,
+    GatewayId=Ref(internet_gateway),
+    DestinationCidrBlock="0.0.0.0/0",
+    RouteTableId=Ref(public_route_table),
 )
