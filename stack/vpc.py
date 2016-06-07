a318fdfc65d76bf705b63e266dd1cef9031ba6c9
jean-philippe serafin <serafinjp@gmail.com>
2016-06-07 13:47:42 +0000

    Add two private subnets (between two zones) that will hold the container instances

--- stack/vpc.py
+++ stack/vpc.py
@@ -147,3 +147,40 @@ private_nat_route = Route(
     DestinationCidrBlock="0.0.0.0/0",
     NatGatewayId=Ref(nat_gateway),
 )
+
+
+# Holds containers instances
+container_a_subnet_cidr = "10.0.10.0/24"
+container_a_subnet = Subnet(
+    "ContainerASubnet",
+    template=template,
+    VpcId=Ref(vpc),
+    CidrBlock=container_a_subnet_cidr,
+    AvailabilityZone=Join("", [Ref(AWS_REGION), "a"]),
+)
+
+
+SubnetRouteTableAssociation(
+    "ContainerARouteTableAssociation",
+    template=template,
+    SubnetId=Ref(container_a_subnet),
+    RouteTableId=Ref(private_route_table),
+)
+
+
+container_b_subnet_cidr = "10.0.11.0/24"
+container_b_subnet = Subnet(
+    "ContainerBSubnet",
+    template=template,
+    VpcId=Ref(vpc),
+    CidrBlock=container_b_subnet_cidr,
+    AvailabilityZone=Join("", [Ref(AWS_REGION), "b"]),
+)
+
+
+SubnetRouteTableAssociation(
+    "ContainerBRouteTableAssociation",
+    template=template,
+    SubnetId=Ref(container_b_subnet),
+    RouteTableId=Ref(private_route_table),
+)
