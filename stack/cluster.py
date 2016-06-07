de2ece8bc4c924cecde4247d75e6b3c209010781
jean-philippe serafin <serafinjp@gmail.com>
2016-06-07 11:31:07 +0000

    Add a `ContainerInstanceType` stack parameter
    Will allow to choose the instance type to run within the `ECS`
    cluster.

--- stack/cluster.py
+++ stack/cluster.py
@@ -1,8 +1,22 @@
+from troposphere import (
+    Parameter,
+    Ref,
+)
+
 from troposphere.ecs import (
     Cluster,
 )
 
 from .template import template
+
+
+container_instance_type = Ref(template.add_parameter(Parameter(
+    "ContainerInstanceType",
+    Description="The container instance type",
+    Type="String",
+    Default="t2.micro",
+    AllowedValues=["t2.micro", "t2.small", "t2.medium"]
+)))
 
 
 # ECS cluster
