e5a103d223976523a5af2c42117ace90a7fa9233
jean-philippe serafin <serafinjp@gmail.com>
2016-06-08 09:07:18 +0000

    Allow connections from container subnets to database instance

--- stack/database.py
+++ stack/database.py
@@ -1,4 +1,5 @@
 from troposphere import (
+    ec2,
     rds,
     Ref,
     AWS_STACK_NAME,
@@ -6,8 +7,34 @@ from troposphere import (
 
 from .template import template
 from .vpc import (
+    vpc,
     container_a_subnet,
+    container_a_subnet_cidr,
     container_b_subnet,
+    container_b_subnet_cidr,
+)
+
+
+db_security_group = ec2.SecurityGroup(
+    'DatabaseSecurityGroup',
+    template=template,
+    GroupDescription="Database security group.",
+    VpcId=Ref(vpc),
+    SecurityGroupIngress=[
+        # Postgres in from web clusters
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort="5432",
+            ToPort="5432",
+            CidrIp=container_a_subnet_cidr,
+        ),
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort="5432",
+            ToPort="5432",
+            CidrIp=container_b_subnet_cidr,
+        ),
+    ],
 )
 
 
@@ -28,5 +55,6 @@ db_instance = rds.DBInstance(
     MultiAZ=True,
     StorageType="gp2",
     DBSubnetGroupName=Ref(db_subnet_group),
+    VPCSecurityGroups=[Ref(db_security_group)],
     BackupRetentionPeriod="7",
 )
