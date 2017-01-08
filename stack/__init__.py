9fc29ad871b559a7a7517b00e11af885a5ef3f57
Tobias McNulty <tobias@caktusgroup.com>
2017-01-08 10:43:50 -0500

    add __init__py to print current configuration based on environment variables set, if any

---
+++ stack/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+from . import assets  # noqa: F401
+from . import cache  # noqa: F401
+from . import certificates  # noqa: F401
+from . import database  # noqa: F401
+from . import domain  # noqa: F401
+from . import repository  # noqa: F401
+from . import vpc  # noqa: F401
+from . import template
+
+if os.environ.get('USE_ECS') == 'on':
+    from . import cluster  # noqa: F401
+else:
+    from . import eb  # noqa: F401
+
+print(template.template.to_json())
