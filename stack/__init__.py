570497fb7d1108b45941c8e3e2f6cd7116693efe
Tobias McNulty <tobias@caktusgroup.com>
2017-10-14 07:41:49 -0400

    clean up conditional imports in __init__.py

--- stack/__init__.py
+++ stack/__init__.py
@@ -7,11 +7,6 @@ from . import logs  # noqa: F401
 from . import vpc  # noqa: F401
 from . import template
 
-if os.environ.get('USE_GOVCLOUD') != 'on':
-    # make sure this isn't added to the template for GovCloud, as it's not
-    # supported in this region
-    from . import search  # noqa: F401
-
 if os.environ.get('USE_ECS') == 'on':
     from . import repository  # noqa: F401
     from . import cluster  # noqa: F401
@@ -23,8 +18,10 @@ elif os.environ.get('USE_DOKKU') == 'on':
 else:  # USE_GOVCLOUD and USE_EC2 both provide EC2 instances
     from . import instances  # noqa: F401
 
-# only non-GovCloud stacks with a load balancer can have a CDN for the app server
-if os.environ.get('USE_GOVCLOUD') != 'on' and os.environ.get('USE_DOKKU') != 'on':
+if os.environ.get('USE_GOVCLOUD') != 'on':
+    # make sure these resources aren't added to the template for GovCloud, as
+    # they aren't supported in this region
+    from . import search  # noqa: F401
     from . import cdn  # noqa: F401
 
 print(template.template.to_json())
