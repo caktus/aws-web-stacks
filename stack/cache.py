199a7453b817b29cb3733254b6c181747fd24798
Colin Copeland <copelco@caktusgroup.com>
2019-09-24 10:43:21 -0400

    split Memcached and Redis into separate services

--- stack/cache.py
+++ stack/cache.py
@@ -6,9 +6,11 @@ from troposphere import (
     If,
     Join,
     Not,
+    Or,
     Output,
     Ref,
     Tags,
+    constants,
     ec2,
     elasticache
 )
@@ -18,93 +20,77 @@ from .common import (
     use_aes256_encryption,
     use_aes256_encryption_cond
 )
+from .security_groups import container_security_group
 from .template import template
 from .utils import ParameterWithDefaults as Parameter
-from .vpc import (
-    private_subnet_a,
-    private_subnet_a_cidr,
-    private_subnet_b,
-    private_subnet_b_cidr,
-    vpc
-)
+from .vpc import private_subnet_a, private_subnet_b, vpc
+
+NODE_TYPES = [
+    dont_create_value,
+    'cache.t2.micro',
+    'cache.t2.small',
+    'cache.t2.medium',
+    'cache.m3.medium',
+    'cache.m3.large',
+    'cache.m3.xlarge',
+    'cache.m3.2xlarge',
+    'cache.m4.large',
+    'cache.m4.xlarge',
+    'cache.m4.2xlarge',
+    'cache.m4.4xlarge',
+    'cache.m4.10xlarge',
+    'cache.r4.large',
+    'cache.r4.xlarge',
+    'cache.r4.2xlarge',
+    'cache.r4.4xlarge',
+    'cache.r4.8xlarge',
+    'cache.r4.16xlarge',
+    'cache.r3.large',
+    'cache.r3.xlarge',
+    'cache.r3.2xlarge',
+    'cache.r3.4xlarge',
+    'cache.r3.8xlarge',
+]
 
 cache_node_type = template.add_parameter(
     Parameter(
         "CacheNodeType",
-        Default="cache.t2.micro",
+        Default=dont_create_value,
         Description="Cache instance type",
         Type="String",
-        AllowedValues=[
-            dont_create_value,
-            'cache.t2.micro',
-            'cache.t2.small',
-            'cache.t2.medium',
-            'cache.m3.medium',
-            'cache.m3.large',
-            'cache.m3.xlarge',
-            'cache.m3.2xlarge',
-            'cache.m4.large',
-            'cache.m4.xlarge',
-            'cache.m4.2xlarge',
-            'cache.m4.4xlarge',
-            'cache.m4.10xlarge',
-            'cache.r4.large',
-            'cache.r4.xlarge',
-            'cache.r4.2xlarge',
-            'cache.r4.4xlarge',
-            'cache.r4.8xlarge',
-            'cache.r4.16xlarge',
-            'cache.r3.large',
-            'cache.r3.xlarge',
-            'cache.r3.2xlarge',
-            'cache.r3.4xlarge',
-            'cache.r3.8xlarge',
-        ],
+        AllowedValues=NODE_TYPES,
         ConstraintDescription="must select a valid cache node type.",
     ),
-    group="Cache",
+    group="Memcached",
     label="Instance Type",
 )
 
-cache_engine = template.add_parameter(
-    Parameter(
-        "CacheEngine",
-        Default="redis",
-        Description="Cache engine (redis or memcached)",
-        Type="String",
-        AllowedValues=[
-            'redis',
-            'memcached',
-        ],
-        ConstraintDescription="must select a valid cache engine.",
-    ),
-    group="Cache",
-    label="Engine",
-)
+using_memcached_condition = "UsingMemcached"
+template.add_condition(using_memcached_condition, Not(Equals(Ref(cache_node_type), dont_create_value)))
 
-cache_condition = "CacheCondition"
-template.add_condition(cache_condition, Not(Equals(Ref(cache_node_type), dont_create_value)))
+redis_node_type = template.add_parameter(
+    Parameter(
+        "RedisNodeType",
+        Default=dont_create_value,
+        Description="Redis instance type",
+        Type="String",
+        AllowedValues=NODE_TYPES,
+        ConstraintDescription="must select a valid cache node type.",
+    ),
+    group="Redis",
+    label="Instance Type",
+)
 
 using_redis_condition = "UsingRedis"
-template.add_condition(
-    using_redis_condition,
-    Equals(Ref(cache_engine), "redis"),
-)
-
-using_memcached_condition = "UsingMemcached"
-template.add_condition(
-    using_memcached_condition,
-    Equals(Ref(cache_engine), "memcached"),
-)
-
+template.add_condition(using_redis_condition, Not(Equals(Ref(redis_node_type), dont_create_value)))
 
 # Parameter constraints (MinLength, AllowedPattern, etc.) don't allow a blank value,
 # so we use a special "blank" do-not-create value
 auth_token_dont_create_value = 'DO_NOT_CREATE_AUTH_TOKEN'
 
-cache_auth_token = template.add_parameter(
+redis_auth_token = template.add_parameter(
     Parameter(
-        "CacheAuthToken",
+        "RedisAuthToken",
         NoEcho=True,
         Default=auth_token_dont_create_value,
         Description="The password used to access a Redis ReplicationGroup (required for HIPAA).",
@@ -115,32 +101,56 @@ cache_auth_token = template.add_parameter(
         ConstraintDescription="must consist of 16-128 printable ASCII "
                               "characters except \"/\", \"\"\", or \"@\"."
     ),
-    group="Cache",
+    group="Redis",
     label="AuthToken",
 )
 
-cache_auth_token_condition = "CacheAuthTokenCondition"
-template.add_condition(cache_auth_token_condition, Not(Equals(Ref(cache_auth_token), auth_token_dont_create_value)))
+using_auth_token_condition = "AuthTokenCondition"
+template.add_condition(using_auth_token_condition,
+                       Not(Equals(Ref(redis_auth_token), auth_token_dont_create_value)))
+
+secure_redis_condition = "SecureRedisCondition"
+template.add_condition(secure_redis_condition,
+                       And(Condition(using_redis_condition), Condition(use_aes256_encryption_cond)))
+
+using_either_cache_condition = "EitherCacheCondition"
+template.add_condition(using_either_cache_condition,
+                       Or(Condition(using_memcached_condition), Condition(using_redis_condition)))
+
+cache_subnet_group = elasticache.SubnetGroup(
+    "CacheSubnetGroup",
+    template=template,
+    Description="Subnets available for the cache instance",
+    Condition=using_either_cache_condition,
+    SubnetIds=[Ref(private_subnet_a), Ref(private_subnet_b)],
+)
 
 cache_security_group = ec2.SecurityGroup(
     'CacheSecurityGroup',
     template=template,
     GroupDescription="Cache security group.",
-    Condition=cache_condition,
+    Condition=using_either_cache_condition,
     VpcId=Ref(vpc),
     SecurityGroupIngress=[
-        # Redis in from web clusters
-        ec2.SecurityGroupRule(
-            IpProtocol="tcp",
-            FromPort=If(using_redis_condition, "6379", "11211"),
-            ToPort=If(using_redis_condition, "6379", "11211"),
-            CidrIp=Ref(private_subnet_a_cidr),
+        If(
+            using_memcached_condition,
+            ec2.SecurityGroupRule(
+                IpProtocol="tcp",
+                FromPort=constants.MEMCACHED_PORT,
+                ToPort=constants.MEMCACHED_PORT,
+                SourceSecurityGroupId=Ref(container_security_group),
+            ),
+            Ref("AWS::NoValue"),
         ),
-        ec2.SecurityGroupRule(
-            IpProtocol="tcp",
-            FromPort=If(using_redis_condition, "6379", "11211"),
-            ToPort=If(using_redis_condition, "6379", "11211"),
-            CidrIp=Ref(private_subnet_b_cidr),
+        If(
+            using_redis_condition,
+            ec2.SecurityGroupRule(
+                IpProtocol="tcp",
+                FromPort=constants.REDIS_PORT,
+                ToPort=constants.REDIS_PORT,
+                SourceSecurityGroupId=Ref(container_security_group),
+            ),
+            Ref("AWS::NoValue"),
         ),
     ],
     Tags=Tags(
@@ -148,23 +158,36 @@ cache_security_group = ec2.SecurityGroup(
     ),
 )
 
-cache_subnet_group = elasticache.SubnetGroup(
-    "CacheSubnetGroup",
+redis_replication_group = elasticache.ReplicationGroup(
+    "RedisReplicationGroup",
     template=template,
-    Description="Subnets available for the cache instance",
-    Condition=cache_condition,
-    SubnetIds=[Ref(private_subnet_a), Ref(private_subnet_b)],
+    AtRestEncryptionEnabled=use_aes256_encryption,
+    AutomaticFailoverEnabled=False,
+    AuthToken=If(using_auth_token_condition, Ref(redis_auth_token), Ref("AWS::NoValue")),
+    Engine="redis",
+    CacheNodeType=Ref(redis_node_type),
+    CacheSubnetGroupName=Ref(cache_subnet_group),
+    Condition=using_redis_condition,
+    NumNodeGroups=1,
+    Port=constants.REDIS_PORT,
+    ReplicationGroupId=Join("-", [Ref("AWS::StackName"), "redis"]),
+    ReplicationGroupDescription="Redis ReplicationGroup",
+    SecurityGroupIds=[Ref(cache_security_group)],
+    TransitEncryptionEnabled=use_aes256_encryption,
+    Tags=Tags(
+        Name=Join("-", [Ref("AWS::StackName"), "redis"]),
+    ),
 )
 
 cache_cluster = elasticache.CacheCluster(
     "CacheCluster",
     ClusterName=Join("-", [Ref("AWS::StackName"), "cache"]),
     template=template,
-    Engine=Ref(cache_engine),
+    Engine="memcached",
     CacheNodeType=Ref(cache_node_type),
     Condition=using_memcached_condition,
-    NumCacheNodes=1,  # Must be 1 for redis, but still required
-    Port=11211,
+    NumCacheNodes=1,
+    Port=constants.MEMCACHED_PORT,
     VpcSecurityGroupIds=[Ref(cache_security_group)],
     CacheSubnetGroupName=Ref(cache_subnet_group),
     Tags=Tags(
@@ -172,88 +195,93 @@ cache_cluster = elasticache.CacheCluster(
     ),
 )
 
-redis_replication_group = elasticache.ReplicationGroup(
-    "CacheReplicationGroup",
-    template=template,
-    AtRestEncryptionEnabled=use_aes256_encryption,
-    AutomaticFailoverEnabled=False,
-    AuthToken=If(cache_auth_token_condition, Ref(cache_auth_token), Ref("AWS::NoValue")),
-    Engine=Ref(cache_engine),
-    CacheNodeType=Ref(cache_node_type),
-    CacheSubnetGroupName=Ref(cache_subnet_group),
-    Condition=using_redis_condition,
-    NumNodeGroups=1,
-    Port=6379,
-    ReplicationGroupId=Join("-", [Ref("AWS::StackName"), "cache"]),
-    ReplicationGroupDescription="Redis ReplicationGroup",
-    SecurityGroupIds=[Ref(cache_security_group)],
-    TransitEncryptionEnabled=use_aes256_encryption,
-    Tags=Tags(
-        Name=Join("-", [Ref("AWS::StackName"), "cache"]),
-    ),
-)
-
-secure_redis_condition = "SecureRedisCondition"
-template.add_condition(secure_redis_condition,
-                       And(Condition(using_redis_condition), Condition(use_aes256_encryption_cond)))
-
 cache_address = If(
-    cache_condition,
-    If(
-        using_redis_condition,
-        GetAtt(redis_replication_group, 'PrimaryEndPoint.Address'),
-        GetAtt(cache_cluster, 'ConfigurationEndpoint.Address')
-    ),
-    "",  # defaults to empty string if no cache was created
+    using_memcached_condition,
+    GetAtt(cache_cluster, 'ConfigurationEndpoint.Address'),
+    "",
 )
 
 cache_port = If(
-    cache_condition,
-    If(
-        using_redis_condition,
-        GetAtt(redis_replication_group, 'PrimaryEndPoint.Port'),
-        GetAtt(cache_cluster, 'ConfigurationEndpoint.Port')
-    ),
-    "",  # defaults to empty string if no cache was created
+    using_memcached_condition,
+    GetAtt(cache_cluster, 'ConfigurationEndpoint.Port'),
+    "",
 )
 
 cache_url = If(
-    cache_condition,
+    using_memcached_condition,
     Join("", [
-        Ref(cache_engine),
-        If(secure_redis_condition, "s", ""),
-        "://",
-        If(cache_auth_token_condition, ":_PASSWORD_@", ""),
+        "memcached://",
         cache_address,
         ":",
-        cache_port
+        cache_port,
     ]),
-    "",  # defaults to empty string if no cache was created
+    "",
 )
-
-template.add_output([
-    Output(
-        "CacheURL",
-        Description="URL to connect to the cache node/cluster.",
-        Value=cache_url,
-        Condition=cache_condition,
-    ),
-])
 
 template.add_output([
     Output(
         "CacheAddress",
         Description="The DNS address for the cache node/cluster.",
         Value=cache_address,
-        Condition=cache_condition,
+        Condition=using_memcached_condition,
     ),
-])
-
-template.add_output([
     Output(
         "CachePort",
         Description="The port number for the cache node/cluster.",
-        Value=cache_port,
-        Condition=cache_condition,
+        Value=GetAtt(cache_cluster, 'ConfigurationEndpoint.Port'),
+        Condition=using_memcached_condition,
+    ),
+    Output(
+        "CacheURL",
+        Description="URL to connect to the cache node/cluster.",
+        Value=cache_url,
+        Condition=using_memcached_condition,
+    ),
+])
+
+redis_address = If(
+    using_redis_condition,
+    GetAtt(redis_replication_group, 'PrimaryEndPoint.Address'),
+    "",
+)
+
+redis_port = If(
+    using_redis_condition,
+    GetAtt(redis_replication_group, 'PrimaryEndPoint.Port'),
+    "",
+)
+
+redis_url = If(
+    using_redis_condition,
+    Join("", [
+        "redis",
+        If(secure_redis_condition, "s", ""),
+        "://",
+        If(using_auth_token_condition, ":_PASSWORD_@", ""),
+        redis_address,
+        ":",
+        redis_port,
+    ]),
+    "",
+)
+
+template.add_output([
+    Output(
+        "RedisAddress",
+        Description="The DNS address for the Redis node/cluster.",
+        Value=redis_address,
+        Condition=using_redis_condition,
+    ),
+    Output(
+        "RedisPort",
+        Description="The port number for the Redis node/cluster.",
+        Value=redis_port,
+        Condition=using_redis_condition,
+    ),
+    Output(
+        "RedisURL",
+        Description="URL to connect to the Redis node/cluster.",
+        Value=redis_url,
+        Condition=using_redis_condition,
     ),
 ])
