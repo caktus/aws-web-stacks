a1b7d16eae0ef9120613ec103042206fc64b0634
Colin Copeland <copelco@caktusgroup.com>
2019-12-06 18:07:16 -0500

    add AWSTransferRole

--- stack/transfer.py
+++ stack/transfer.py
@@ -1,5 +1,6 @@
 from troposphere import (
     Equals,
+    iam,
     Join,
     Parameter,
     Ref,
@@ -7,6 +8,10 @@ from troposphere import (
     transfer,
 )
 
+from .assets import private_assets_bucket
+from .common import (
+    arn_prefix,
+)
 from .template import template
 
 use_transfer_server = template.add_parameter(
@@ -33,4 +38,48 @@ transfer_server = transfer.Server(
     Tags=Tags(
         Name=Join("-", [Ref("AWS::StackName"), "sftp"]),
     ),
+)
+
+sftp_rw_policy = iam.Policy(
+    PolicyName="SFTPS3RWPolicy",
+    PolicyDocument=dict(
+        Statement=[
+            dict(
+                Effect="Allow",
+                Action=[
+                    "s3:ListBucket",
+                    "s3:GetBucketLocation",
+                ],
+                Resource=Join("", [arn_prefix, ":s3:::", Ref(private_assets_bucket)]),
+            ),
+            dict(
+                Effect="Allow",
+                Action=[
+                    "s3:PutObject",
+                    "s3:GetObject",
+                    "s3:DeleteObject",
+                    "s3:DeleteObjectVersion",
+                    "s3:GetObjectVersion",
+                    "s3:GetObjectACL",
+                    "s3:PutObjectACL",
+                ],
+                Resource=Join("", [arn_prefix, ":s3:::", Ref(private_assets_bucket), "/*"]),
+            ),
+        ],
+    ),
+)
+
+transfer_role = iam.Role(
+    "AWSTransferRole",
+    template=template,
+    Condition=use_transfer_server_condition,
+    AssumeRolePolicyDocument=dict(Statement=[dict(
+        Effect="Allow",
+        Principal=dict(Service=["transfer.amazonaws.com"]),
+        Action=["sts:AssumeRole"],
+    )]),
+    Policies=[
+        sftp_rw_policy,
+    ],
+    RoleName=Join("-", [Ref("AWS::StackName"), "AWSTransferRole"]),
 )
